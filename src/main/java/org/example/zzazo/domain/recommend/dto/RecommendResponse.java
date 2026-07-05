153d1e826b78217db6b4f455228860b96c1f8804
Nanajun22 <skaudwns3@gmail.com>
2026-07-05 21:14:48 +0900

    docs: 시간표 추천 응답 Dto 명세 작성

--- src/main/java/org/example/zzazo/domain/recommend/dto/RecommendResponse.java
+++ src/main/java/org/example/zzazo/domain/recommend/dto/RecommendResponse.java
@@ -1,6 +1,7 @@
 package org.example.zzazo.domain.recommend.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.example.zzazo.domain.lecture.domain.LectureClassification;
 import org.example.zzazo.global.common.Week;
 
@@ -8,27 +9,43 @@ import java.time.LocalTime;
 import java.util.List;
 
 public class RecommendResponse {
+
+    @Schema(description = "시간표 추천 결과")
     public record RecommendResult(
+            @Schema(description = "추천 시간표의 총 학점", example = "18")
             int totalCredits,
-            Week[] satisfiedFreeDays,
+            @Schema(
+                    description = "추천 결과 시간표 공강 요일",
+                    example = "[\"FRI\"]"
+            )
+            List<Week> satisfiedFreeDays,
+            @Schema(description = "추천 시간표의 강의 목록")
             List<lecture> timetables
     ) {
 
     }
 
     public record lecture(
+            @Schema(description = "강의 ID",example = "1")
             Long lectureId,
+            @Schema(description = "강의명",example = "경영학원론")
             String lectureName,
+            @Schema(description = "학점",example = "3")
             int credit,
+            @Schema(description = "강의 분류 [전공필수,전공선택,교양필수,교양선택]",example = "전공필수")
             LectureClassification category,
+            @Schema(description = "강의시간")
             List<LectureTime> lectureTime
             ) {
         public record LectureTime(
-                Week day,
+                @Schema(description = "강의 시작시간",example = "13:00")
                 @JsonFormat(pattern = "HH:mm")
                 LocalTime startTime,
+                @Schema(description = "강의 종료시간",example = "14:00")
                 @JsonFormat(pattern = "HH:mm")
-                LocalTime endTime
+                LocalTime endTime,
+                @Schema(description = "강의 요일",example = "MON")
+                Week week
         ) {
 
         }
