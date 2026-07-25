6a44edb7166c7b032cbad650739b860555716939
변승현 <qustmdgus1212@gmail.com>
2026-07-26 00:28:34 +0900

    feat: 시간표 연관 강의 삭제 쿼리 추가

--- src/main/java/org/example/zzazo/domain/timetable/repository/TimetableLectureRepository.java
+++ src/main/java/org/example/zzazo/domain/timetable/repository/TimetableLectureRepository.java
@@ -2,6 +2,13 @@ package org.example.zzazo.domain.timetable.repository;
 
 import org.example.zzazo.domain.timetable.entity.TimetableLecture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TimetableLectureRepository extends JpaRepository<TimetableLecture, Long> {
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("delete from TimetableLecture tl where tl.timetable.timetableId = :timetableId")
+    void deleteAllByTimetableId(@Param("timetableId") Long timetableId);
 }
