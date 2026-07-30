d0882019db45d22c865805796eb3de25b94e0e5d
Nanajun22 <skaudwns3@gmail.com>
2026-07-31 04:43:55 +0900

    chore: 도커 컴포즈 환경 수정

---
+++ src/main/resources/db/seed/dev/V1_0_1__department.sql
@@ -0,0 +1,8 @@
+-- ============================================
+-- 학과 (2개)
+-- ============================================
+
+INSERT INTO department (department_id, department_name, created_at, updated_at)
+VALUES
+    (1, '경영학과', NOW(), NOW()),
+    (2, '컴퓨터공학과', NOW(), NOW());
